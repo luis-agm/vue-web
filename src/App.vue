7ea6bae5e3d6ef8067be9a7f56b431e7be8f0af9
Luis González <lgonzalez@teravisiontech.com>
2017-06-10 16:31:13 -0400

    Add vuetify to see if I like it

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
   <div id="app">
-    <div id='header'>
-      <h1>{{ headerTitle }}</h1>
-    </div>
+    <main-header></main-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import 'particles.js'
-import partCfg from '@/assets/particles.config.json'
+import MainHeader from 'components/Header.vue'
 
 export default {
   name: 'app',
-  mounted () {
-    console.log(partCfg)
-    window.particlesJS('header', partCfg)
-  },
-  data () {
-    return {
-      headerTitle: 'Awesome Header'
-    }
-  }
+  components: {MainHeader}
 }
 </script>
 
@@ -34,39 +23,9 @@ body {
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
-#header {
-  background: url('assets/roraima.jpg');
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  height: 800px;
-  width: 100%;
-
-  h1 {
-    font-family: 'Ubuntu', sans-serif;
-    z-index: 99;
-    align-self: center;
-    color: #FFF;
-    top: 0;
-    width: 20%;
-  }
-
-  canvas {
-    background-color: rgba(#4781FF,0.2);
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-}
-
-/* flexbox mebby? */
-
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id='header'>
+    <v-toolbar class='blue'>
+      <span>hello</span>
+    </v-toolbar>
+    <div id='parts'></div>
+    <h1>{{ headerTitle }}</h1>
+  </div>
+</template>
+
+<script>
+import 'particles.js'
+import partCfg from '@/assets/particles.config.json'
+
+export default {
+  name: 'MainHeader',
+  mounted () {
+    console.log(partCfg)
+    window.particlesJS('header', partCfg)
+  },
+  data () {
+    return {
+      headerTitle: 'Awesome Header'
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+
+#header {
+  background: url('../assets/roraima.jpg');
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-flow: column;
+  height: 800px;
+  width: 100%;
+
+  .toolbar {
+    width: 100%;
+  }
+
+  h1 {
+    font-family: 'Ubuntu', sans-serif;
+    z-index: 99;
+    align-self: flex-start;
+    color: #FFF;
+    top: 0;
+    width: 20%;
+  }
+
+  canvas.particles-js-canvas-el {
+    /* background-color: rgba(#4781FF,0.2); */
+    background-color: rgba(255,255,255,0.6);
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+
+</style>
