93533575a477ddad7e51634cd0cbf7160062743a
Luis González <lgonzalez@teravisiontech.com>
2017-06-27 11:42:15 -0400

    Footer now appears

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -12,8 +12,10 @@
 
 <style lang='scss'>
   .footer {
-    align-self: flex-end;
+    align-self: center;
+    position:absolute;
     bottom: 0;
+    background: transparent;
     &__text {
       width: 100%;
       font-size: 0.8em;
